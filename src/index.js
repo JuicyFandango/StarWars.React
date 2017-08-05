6ce9dc09ef8773b4ea32aff4cf4dd44cd7fda50a
AgustinGonzalez <xenrak@gmail.com>
2017-08-05 14:16:06 -0400

    Preparación para desafío

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Button, Icon, Collection} from 'react-materialize';
 import './index.css';
 import Galaxia from './componentes/galaxia'
 import ListaJedi from './componentes/listaJedi'
+// Archivo de configuración
 import Config from './config.js'
 
 class App extends React.Component {
@@ -23,15 +24,7 @@ class App extends React.Component {
 
   _obtenerJedis() {
     return new Promise( (resolve, reject) => {
-      fetch(`${Config.API_URL}/api/occupation/jedi/people`)
-        .then( response => {
-          response.json().then( body => {
-            resolve(body);
-          })
-        })
-        .catch( (error) => {
-          reject(`Error al obtener jedis: ${error}`);
-        })
+      resolve([])
     })
   }
 
@@ -84,7 +77,7 @@ class App extends React.Component {
             break;
           }
           const indicePlanetaArrastre = planetas.findIndex( planeta => planeta.nombre === this.state.arrastre.lugar );
-          
+
           if(indicePlanetaArrastre !== -1) {
             planetas[indicePlanetaArrastre].jedi = null;
           } else {
@@ -100,9 +93,12 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // Obtención de Jedis desde la API
     this._obtenerJedis().then( jedis => {
       this.setState({ jedis : jedis })
     });
+
+    // Obtención de planetas !!!
     this._obtenerPlanetas().then( planetas => {
       this.setState({ planetas : planetas })
     });
@@ -110,36 +106,35 @@ class App extends React.Component {
 
   render() {
     return (
- <div className="App">
+      // Contenedor principal de la aplicación
+      <div className="App">
         <nav className="black">
-    <div className="nav-wrapper">
-       <img width="100" height="85" src="https://www.merchoid.com/wp-content/uploads/2016/02/xreflogo-starwars.jpg.pagespeed.ic.-cU0EJSd2O.jpg" className="App-logo" alt="logo" />
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-     
-      </ul>
-    </div>
-  </nav>
+          <div className="nav-wrapper">
+             <img width="100" height="85" src="https://www.merchoid.com/wp-content/uploads/2016/02/xreflogo-starwars.jpg.pagespeed.ic.-cU0EJSd2O.jpg" className="App-logo" alt="logo" />
+             <ul id="nav-mobile" className="right hide-on-med-and-down">
+             </ul>
+          </div>
+        </nav>
 
-      
        <div className="container">
-         <div className="row"> 
+         <div className="row">
            <h3 className="center">Star Wars Commander</h3>
-      <h5 className="center">Arrastra los jedis hacia los planetas para calcular el porcentaje de exito de la misión</h5>
-      <container>
-        <br/>
-        <ListaJedi
-          jedis={this.state.jedis}
-          moverJedi={this._moverJedi}
-          transferirJedi={this._transferirJedi}
-        />
-        <Galaxia
-          planetas={this.state.planetas}
-          moverJedi={this._moverJedi}
-          transferirJedi={this._transferirJedi}
-        /> 
-      </container>
-      </div>
-      </div>
+           <h5 className="center">Arrastra los jedis hacia los planetas para calcular el porcentaje de exito de la misión</h5>
+           <container>
+             <br/>
+             <ListaJedi
+              jedis={this.state.jedis}
+              moverJedi={this._moverJedi}
+              transferirJedi={this._transferirJedi}
+              />
+              <Galaxia
+                planetas={this.state.planetas}
+                moverJedi={this._moverJedi}
+                transferirJedi={this._transferirJedi}
+              />
+            </container>
+          </div>
+        </div>
       </div>
     );
   }
